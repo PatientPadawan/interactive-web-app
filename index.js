f61b580e891d0347b752c17511025fb06e3681c1
Patient Padawan <1PatientPadawan@gmail.com>
2019-04-25 14:21:01 -0700

    added jQuery library and delete functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+$(function() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const userTextInput = $(this).find('#shopping-list-entry');
+        // empty #shopping-list-entry
+
+        $('ul').append(
+            `<li>
+                <span class="shopping-item">${userTextInput}</span>
+                <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+                </div>
+            </li>`
+        );
+    });
+
+    $('li').on('click', '.shopping-item-delete', function(event) {
+        $(this).parentsUntil("ul").remove();
+    });
+});
